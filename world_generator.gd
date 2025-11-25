8c8ffec6a8a4c5fafa485a754f5e32bbec22736a
ali.saylisoy <alican.saylisoy@tu-dortmund.de>
2025-11-25 10:46:09 +0100

    Implemented a noise algorithm that builds main base castle. To be continued

--- world_generator.gd
+++ world_generator.gd
@@ -2,7 +2,7 @@ extends Node3D
 
 #artik bir kare degil, merkezden yayilan bir petek yapisi var
 #bu sayi, en merkezden en distaki petege kadar kac petek olacagini belirler
-@export var world_radius: int = 100
+@export var world_radius: int = 10
 #tile_size yerine hex_size kullaniyoruz
 #bu, altigen modelinin merkezinden sivri tepesine kadar olan uzakligidir
 @export var hex_size: float = 1.0
@@ -12,11 +12,18 @@ extends Node3D
 #üretilen veriyi saklama -bu da ayni kaliyor- 
 #sadece icindeki anahtarlar artik (x,z) degil, (q,r) olacak
 var height_data = {}
+var main_base_location: Vector2i
 
 func _ready():
 	generate_world_data()
 	#...diger fonksiyonlar daha eklenecek
 	print("altigen dünya veri haritasi olusturuldu.")
+	
+	place_fixed_structures()#TODO
+	
+	build_the_world()#TODO
+	
+	print("Altigen dünya olusturma tamamlandi")
 	
 func generate_world_data():
 	print("yükseklik verisi üretiliyor...")
@@ -65,10 +72,46 @@ const HEX_DIRECTIONS = [
 	Vector2i(-1, 0), Vector2i(-1, 1), Vector2i(0, 1)
 ]
 
-func get_hex_area(center, radius):#TODO
+func get_distance(hexA: Vector3i, hexB: Vector3i) -> int:
+	var hexA_q = hexA.x
+	var hexA_r = hexA.y
+	var hexA_s = hexA.z
+	var hexB_q = hexB.x
+	var hexB_r = hexB.y
+	var hexB_s = hexB.z
+	var delta_q = abs(hexA_q - hexB_q)
+	var delta_r = abs(hexA_r - hexB_r)
+	var delta_s = abs(hexA_s - hexB_s)
+	var distance = ( (delta_q + delta_r + delta_s) / 2 )
+	return distance
 
-func get_neighbors(center):
-	
+func axial_to_cube(axial: Vector2i) -> Vector3i:
+	var q = axial.x
+	var r = axial.y
+	var s = - q - r
+	return Vector3i(q, r, s)
+
+func get_hex_area(center: Vector2i, radius) -> Array:
+	var hexes_found = []
+	var center_q = center.x
+	var center_r = center.y
+	#for loop does not include the upper bound, that is why radius + 1
+	for q in range(center_q - radius, center_q + radius + 1):
+		for r in range(center_r - radius, center_r + radius + 1):
+			var current_hex = Vector2i(q, r)
+			if get_distance(axial_to_cube(center), axial_to_cube(current_hex)) <= radius:
+				hexes_found.append(current_hex)
+	return hexes_found
+
+func get_neighbors(q, r) -> Array:
+	var neighbors = []
+	neighbors.append(Vector2i(q + 1, r))
+	neighbors.append(Vector2i(q, r + 1))
+	neighbors.append(Vector2i(q - 1, r + 1))
+	neighbors.append(Vector2i(q - 1, r))
+	neighbors.append(Vector2i(q, r - 1))
+	neighbors.append(Vector2i(q + 1, r - 1))
+	return neighbors
 
 func place_fixed_structures():
 	#bu satir, her seferinde farkli rastgele sayilar üretilmesini saglar
@@ -92,6 +135,9 @@ func place_fixed_structures():
 	if not suitable_locations.is_empty():
 		#eger en az bir uygun yer bulunduysa onlardan birini rastgele sec
 		base_grid_pos = suitable_locations.pick_random()
+		
+		##################################
+		main_base_location = base_grid_pos
 	else:
 		#hic uygun yer bulunmadiysa merkezi sec ve uyari ver
 		print("UYARI: Üs kurmaya uygun yer bulunamadi, base merkeze kuruldu!")
@@ -101,6 +147,58 @@ func place_fixed_structures():
 	if not height_data.has(base_grid_pos): return
 	var height_at_base = height_data[base_grid_pos]
 	var flatten_radius = 3
-	var area_to_flatten = get_hex_area#TODO
+	var area_to_flatten = get_hex_area(base_grid_pos, flatten_radius)
+	
+	for hex_pos in area_to_flatten:
+		if height_data.has(hex_pos):
+			height_data[hex_pos] = height_at_base
+	
+	
+#param scene is the scene that will be initiated such as buildings, tiles...
+#param grid_pos is the position 
+func place_hex_tile(scene, grid_pos: Vector2i, height: float):
+	if height_data.has(grid_pos):
+		var q = grid_pos.x
+		var r = grid_pos.y
+		height = height_data[grid_pos]
+
+
+func build_the_world():
+	if height_data.has(main_base_location):
+		var q = main_base_location.x
+		var r = main_base_location.y
+		var height = height_data[main_base_location]
+		#one unit movement in q, changes x value sqrt(3) to right
+		#and one unit movement in r, changes x value sqrt(3)/2 to right
+		var world_x = hex_size * (sqrt(3) * q + sqrt(3) / 2.0 * r)
+		#one unit movement in r, changes z value 3/2 downwards
+		#moving one step in the r direction changes the z axis value by itself
+		#it makes a 60 degree angle downwards with the x axis
+		#q movement does not affect the z axis
+		var world_z = hex_size * (3.0 / 2.0 * r)
+		
+		
+		####################################################
+		var castle_instance = MAIN_CASTLE_SCENE.instantiate()
+		add_child(castle_instance)
+		#Vector3 suits better, floating numbers needed for a more delicate and smooth positioning
+		castle_instance.position = Vector3(world_x, height, world_z)
+	else:
+		print("Warning: Main building could not be built, its location could not be found on the map")
+	
+	
+	for grid_pos in height_data.keys():
+		var height = height_data[grid_pos]
+		if height < 0:
+			var water_instance = HEX_WATER_SCENE.instantiate()
+			add_child(water_instance)
+		elif height >= 0 and height < 10:
+			var grass_instance = HEX_GRASS_SCENE.instantiate()
+			add_child(grass_instance)
+		else:
+			var dirt_instance = HEX_DIRT_SCENE.instantiate()
+			add_child(dirt_instance)
+		
+	
 	
 	
